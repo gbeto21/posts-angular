bfb18cfa71b8703ece72b3eb0ead70230590601e
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-31 18:26:49 -0500

    The funtionality of add a new post was implemented through the events.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'posts-angular';
+  storedPosts = []
+
+  onPostAdded(post) {
+    this.storedPosts.push(post)
+  }
+
 }
