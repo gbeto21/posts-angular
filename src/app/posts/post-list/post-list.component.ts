66dad8ab8d3872dfacf9603646a06ccbf8947c08
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-01 07:03:06 -0500

    An new post service was created.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Post } from "../post.model";
+import { PostService } from "../posts.service";
 
 @Component({
   selector: 'app-post-list',
@@ -15,4 +16,9 @@ export class PostListComponent {
   // ]
 
   @Input() posts: Post[] = []
+
+  constructor(public postsService: PostService) {
+
+  }
+
 }
