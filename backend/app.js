6c6d2d6541c49765cf94c73e5a58192f6ffb5bdd
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-01 08:32:43 -0500

    The server returns dummy posts.

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fjfajs',
+      title: 'First post.',
+      content: 'From the server'
+    },
+    {
+      id: 'sldkjlasd',
+      title: 'Second post.',
+      content: 'Second from the server'
+    },
+  ]
 
+  res.status(200).json({
+    message: 'Post fetched succesfuly!',
+    post: posts
+  })
 })
 
 module.exports = app
