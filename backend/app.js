fc0b31cc68f800e9d0e45cc0d2584103e68f1a5b
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-02 08:08:13 -0500

    The get funtionality from the database was implemented.

--- backend/app.js
+++ backend/app.js
@@ -47,23 +47,16 @@ app.post("/api/posts", (req, res, next) => {
 })
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fjfajs',
-      title: 'First post.',
-      content: 'From the server'
-    },
-    {
-      id: 'sldkjlasd',
-      title: 'Second post.',
-      content: 'Second from the server'
-    },
-  ]
 
-  res.status(200).json({
-    message: 'Post fetched succesfuly!',
-    posts: posts
-  })
+  Post
+    .find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Post fetched succesfuly!',
+        posts: documents
+      })
+    })
+
 })
 
 module.exports = app
