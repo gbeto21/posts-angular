2cbf7a38f1cb7cfc9501202634668f3497741cbc
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-02 06:45:37 -0500

    Body parcer was added and implemented on the file app.js of the backend.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -13,6 +16,16 @@ app.use((req, res, next) => {
     "GET, POST, PATH, DELETE, OPTIONS"
   )
   next()
+})
+
+app.post("/api/posts", (req, res, next) => {
+  const posts = req.body
+  console.log(post);
+  res.status(201).json(
+    {
+      message: 'Post added successfully'
+    }
+  )
 })
 
 app.use('/api/posts', (req, res, next) => {
