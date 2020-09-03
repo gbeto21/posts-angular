00bb63bfb5905238ebe83c166393ebb9138ef9ee
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-03 17:27:09 -0500

    The pagination funtionality was modified on the front end.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -21,6 +21,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   isLoading = false
   totalPosts = 10
   postsPerPage = 2
+  currentPage = 1
   pageSizeOptions = [1, 2, 5, 10]
   private postSub: Subscription
 
@@ -30,7 +31,7 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.isLoading = true
-    this.postsService.getPosts()
+    this.postsService.getPosts(this.postsPerPage, this.currentPage)
     this.postSub = this.postsService
       .getPostUpdateListener()
       .subscribe((posts: Post[]) => {
@@ -44,7 +45,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   onChangedPage(pageData: PageEvent) {
-
+    this.currentPage = pageData.pageIndex + 1
+    this.postsPerPage = pageData.pageSize
+    this.postsService.getPosts(this.postsPerPage, this.currentPage)
   }
 
   onDelete(postId: string) {
