1a1df41dae603faed08d8be246199c695c63b164
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-03 16:47:35 -0500

    The upload image funtionality was completed.

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -8,7 +8,8 @@ const postSchema = mongoose.Schema({
   content: {
     type: String,
     required: true
-  }
+  },
+  imagePath: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Post', postSchema)
