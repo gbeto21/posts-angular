d94dcf9f2cc0855246bd915c232b6430e5f060af
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-03 08:31:48 -0500

    Reactive forms were used.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from '@angular/material/card';
@@ -25,7 +25,7 @@ import { HeaderComponent } from './header/header.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
