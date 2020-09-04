3866c68e93f804f0bc1cf0b41ded383cca3ce918
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-04 07:37:44 -0500

    An authorization interceptor was added to the http requests of the front end.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from "@angular/material/button";
@@ -17,6 +17,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { HeaderComponent } from './header/header.component'
 import { LoginComponent } from './auth/login/login.component'
 import { SignupComponent } from './auth/signup/signup.component'
+import { AuthInterceptor } from "./auth/auth-interceptor";
 
 @NgModule({
   declarations: [
@@ -42,7 +43,7 @@ import { SignupComponent } from './auth/signup/signup.component'
     MatPaginatorModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,7 +7,13 @@ import { AuthData } from "./auth-data.model";
 })
 export class AuthService {
 
+  private token: string
+
   constructor(private http: HttpClient) { }
+
+  getToken() {
+    return this.token
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password }
@@ -22,9 +28,10 @@ export class AuthService {
 
     const authData: AuthData = { email: email, password: password }
     this.http
-      .post('http://localhost:3000/api/user/login', authData)
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token
+        this.token = token
       })
   }
 
