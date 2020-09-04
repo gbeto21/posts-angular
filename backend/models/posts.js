ae0e769ecd50e1e48a09256b407bc32ba822e460
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-04 11:37:49 -0500

    The post Schema was related to the user schema and some post images were deleted.

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -9,7 +9,12 @@ const postSchema = mongoose.Schema({
     type: String,
     required: true
   },
-  imagePath: { type: String, required: true }
+  imagePath: { type: String, required: true },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
 })
 
 module.exports = mongoose.model('Post', postSchema)
