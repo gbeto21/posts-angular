aed1fb7410e61c6c63f6dc204c6f370d05bd0a7c
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-04 13:58:33 -0500

    The protect funtionality was completed from the front size.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -25,6 +25,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   currentPage = 1
   pageSizeOptions = [1, 2, 5, 10]
   userIsAuthenticated = false
+  userId: string
   private postSub: Subscription
   private authStatusSub: Subscription
 
@@ -35,6 +36,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.isLoading = true
     this.postsService.getPosts(this.postsPerPage, this.currentPage)
+    this.userId = this.authService.getUserId()
     this.postSub = this.postsService
       .getPostUpdateListener()
       .subscribe((postsData: { posts: Post[], postCount: number }) => {
@@ -49,6 +51,7 @@ export class PostListComponent implements OnInit, OnDestroy {
       .getAuthStatusListener()
       .subscribe(isAuthenticated => {
         this.userIsAuthenticated = isAuthenticated
+        this.userId = this.authService.getUserId()
       })
 
   }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -45,7 +45,14 @@ export class PostService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string; title: string; content: string; imagePath: string }>('http://localhost:3000/api/posts/' + id)
+    return this.http.get<
+      {
+        _id: string;
+        title: string;
+        content: string;
+        imagePath: string;
+        creator: string;
+      }>('http://localhost:3000/api/posts/' + id)
   }
 
   addPost(title: string, content: string, image: File) {
@@ -80,7 +87,8 @@ export class PostService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       }
     }
 
