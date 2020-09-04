2de35954b1229a78955015005b110bcb9cdf7d14
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-04 06:52:29 -0500

    The login request was implemented on the front end.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -35,6 +35,7 @@ router.post("/signup", (req, res, next) => {
 })
 
 router.post("/login", (req, res, next) => {
+  let fetchedUser
   User.findOne({
     email: req.body.email
   })
@@ -44,6 +45,7 @@ router.post("/login", (req, res, next) => {
           message: "Auth failed"
         })
       }
+      fetchedUser = user
       return bcryp.compare(req.body.password, user.password)
     })
     .then(result => {
@@ -54,12 +56,14 @@ router.post("/login", (req, res, next) => {
       }
 
       const token = jwt.sign(
-        { email: user.email, userId: user._id },
+        { email: fetchedUser.email, userId: fetchedUser._id },
         'secret_this_should_be_longer',
         { expiresIn: '1h' }
       )
 
-
+      res.status(200).json({
+        token: token
+      })
 
     }).catch(err => {
       return res.status(401).json({
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from "../auth.service";
+
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
@@ -9,7 +11,12 @@ export class LoginComponent {
 
   isLoading = false
 
-  onLogin(form: NgForm) {
+  constructor(public authService: AuthService) { }
 
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return
+    }
+    this.authService.login(form.value.email, form.value.password)
   }
 }
