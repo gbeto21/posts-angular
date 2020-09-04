4a6ce84879aa8a86bd0dc814640ce633eeda5279
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-03 19:23:23 -0500

    The funtionality of post an user was implemented on the backend.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,34 @@
 const express = require("express")
+const bcryp = require("bcrypt")
 const router = express.Router()
+const User = require('../models/user')
 
 router.post("/signup", (req, res, next) => {
 
+  bcryp
+    .hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: req.body.password
+      })
+      user.save()
+        .then(result => {
+
+          res
+            .status(201)
+            .json(
+              {
+                message: 'User created',
+                result: result
+              })
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          })
+        })
+    })
 })
 
 module.exports = router
